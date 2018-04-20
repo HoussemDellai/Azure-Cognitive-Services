cb92f4363c5a712d0210fe0d7792d659416f14cd
Houssem Dellai <houssem.dellai@live.com>
2018-04-20 16:10:17 +0200

    Add project files.

---
+++ ComputerVisionApplication/ComputerVisionApplication/Models/EmotionResult.cs
@@ -0,0 +1,144 @@
+﻿namespace ComputerVisionApplication.Models.Emotion
+{
+
+    //public class EmotionResult
+    //{
+    //    public FaceRectangle FaceRectangle { get; set; }
+    //    public Scores Scores { get; set; }
+    //}
+
+    //public class FaceRectangle
+    //{
+    //    public int Left { get; set; }
+    //    public int Top { get; set; }
+    //    public int Width { get; set; }
+    //    public int Height { get; set; }
+    //}
+
+    //public class Scores
+    //{
+    //    public float Anger { get; set; }
+    //    public float Contempt { get; set; }
+    //    public float Disgust { get; set; }
+    //    public float Fear { get; set; }
+    //    public float Happiness { get; set; }
+    //    public float Neutral { get; set; }
+    //    public float Sadness { get; set; }
+    //    public float Surprise { get; set; }
+    //}
+
+    //public class Rootobject
+    //{
+    //    public Class1[] Property1 { get; set; }
+    //}
+
+    public class EmotionResult
+    {
+        public string FaceId { get; set; }
+        public Facerectangle FaceRectangle { get; set; }
+        public Faceattributes FaceAttributes { get; set; }
+    }
+
+    public class Facerectangle
+    {
+        public int Top { get; set; }
+        public int Left { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+    }
+
+    public class Faceattributes
+    {
+        public float Smile { get; set; }
+        public Headpose HeadPose { get; set; }
+        public string Gender { get; set; }
+        public float Age { get; set; }
+        public Facialhair FacialHair { get; set; }
+        public string Glasses { get; set; }
+        public Emotion Emotion { get; set; }
+        public Blur Blur { get; set; }
+        public Exposure Exposure { get; set; }
+        public Noise Noise { get; set; }
+        public Makeup Makeup { get; set; }
+        public Accessory[] Accessories { get; set; }
+        public Occlusion Occlusion { get; set; }
+        public Hair Hair { get; set; }
+    }
+
+    public class Headpose
+    {
+        public float Pitch { get; set; }
+        public float Roll { get; set; }
+        public float Yaw { get; set; }
+    }
+
+    public class Facialhair
+    {
+        public float Moustache { get; set; }
+        public float Beard { get; set; }
+        public float Sideburns { get; set; }
+    }
+
+    public class Emotion
+    {
+        public float Anger { get; set; }
+        public float Contempt { get; set; }
+        public float Disgust { get; set; }
+        public float Fear { get; set; }
+        public float Happiness { get; set; }
+        public float Neutral { get; set; }
+        public float Sadness { get; set; }
+        public float Surprise { get; set; }
+    }
+
+    public class Blur
+    {
+        public string BlurLevel { get; set; }
+        public float Value { get; set; }
+    }
+
+    public class Exposure
+    {
+        public string ExposureLevel { get; set; }
+        public float Value { get; set; }
+    }
+
+    public class Noise
+    {
+        public string NoiseLevel { get; set; }
+        public float Value { get; set; }
+    }
+
+    public class Makeup
+    {
+        public bool EyeMakeup { get; set; }
+        public bool LipMakeup { get; set; }
+    }
+
+    public class Occlusion
+    {
+        public bool ForeheadOccluded { get; set; }
+        public bool EyeOccluded { get; set; }
+        public bool MouthOccluded { get; set; }
+    }
+
+    public class Hair
+    {
+        public float Bald { get; set; }
+        public bool Invisible { get; set; }
+        public Haircolor[] HairColor { get; set; }
+    }
+
+    public class Haircolor
+    {
+        public string Color { get; set; }
+        public float Confidence { get; set; }
+    }
+
+    public class Accessory
+    {
+        public string Type { get; set; }
+        public float Confidence { get; set; }
+    }
+
+}
---
+++ ComputerVisionApplication/ComputerVisionApplication/Services/EmotionService.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using CognitiveServices.Models;
+using ComputerVisionApplication.Models;
+using ComputerVisionApplication.Models.Emotion;
+using Newtonsoft.Json;
+
+namespace CognitiveServices.Services
+{
+    /// <summary>
+    /// Client for Computer Vision API (Microsoft Cognitive Services).
+    /// </summary>
+    public class EmotionService
+    {
+
+        /// <summary>
+        /// Get a subscription key from:
+        /// https://www.microsoft.com/cognitive-services/en-us/subscriptions
+        /// </summary>
+        private readonly string _key = "2a180527dd884af38ef011bf2879cf1a";
+        /// <summary>
+        /// Documentation: https://dev.projectoxford.ai/docs/services/5639d931ca73072154c1ce89/operations/563b31ea778daf121cc3a5fa
+        /// </summary>
+        private readonly string _recognizeEmotionsUri = "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect" +
+                                                        "?returnFaceId=true&returnFaceLandmarks=false" +
+                                                        "&returnFaceAttributes=age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise";
+
+        /// <summary>
+        /// Get a subscription key from:
+        /// https://www.microsoft.com/cognitive-services/en-us/subscriptions
+        /// </summary>
+        /// <param name="key">subscription key: required to access the API</param>
+        public EmotionService(string key)
+        {
+            _key = key;
+        }
+        /// <summary>
+        /// Recognizes the emotions expressed by one or more people in an image, 
+        /// as well as returns a bounding box for the face. 
+        /// The emotions detected are happiness, sadness, surprise, anger, fear, 
+        /// contempt, and disgust or neutral. 
+        /// </summary>
+        /// <param name="imageUrl">The image url.</param>
+        /// <returns></returns>
+        public async Task<List<EmotionResult>> RecognizeEmotionsFromImageUrlAsync(string imageUrl)
+        {
+            var httpClient = new HttpClient();
+
+            httpClient.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", _key);
+
+            var stringContent = new StringContent(@"{""url"":""" + imageUrl + @"""}");
+
+            stringContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            try
+            {
+                var response = await httpClient.PostAsync(_recognizeEmotionsUri, stringContent);
+
+                var json = await response.Content.ReadAsStringAsync();
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var imageResultEmotions = JsonConvert.DeserializeObject<List<EmotionResult>>(json);
+
+                    return imageResultEmotions;
+                }
+
+                throw new Exception(json);
+            }
+            catch (Exception exception)
+            {
+                throw exception;
+            }
+        }
+
+        /// <summary>
+        /// Recognizes the emotions expressed by one or more people in an image, 
+        /// as well as returns a bounding box for the face. 
+        /// The emotions detected are happiness, sadness, surprise, anger, fear, 
+        /// contempt, and disgust or neutral. 
+        /// </summary>
+        /// <param name="stream">The image stream.</param>
+        /// <returns></returns>
+        public async Task<List<EmotionResult>> RecognizeEmotionsFromImageStreamAsync(Stream stream)
+        {
+            var httpClient = new HttpClient();
+
+            httpClient.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", _key);
+
+            var streamContent = new StreamContent(stream);
+
+            streamContent.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+
+            try
+            {
+                var response = await httpClient.PostAsync(_recognizeEmotionsUri, streamContent);
+
+                var json = await response.Content.ReadAsStringAsync();
+
+                if (response.IsSuccessStatusCode)
+                {
+
+                    var imageResultEmotions = JsonConvert.DeserializeObject<List<EmotionResult>>(json);
+
+                    return imageResultEmotions;
+                }
+
+                throw new Exception(json);
+            }
+            catch (Exception exception)
+            {
+                throw exception;
+            }
+        }
+    }
+}
