50fcad35ead2b87147865c188a786ddda89c3af9
Houssem Dellai <houssem.dellai@live.com>
2018-04-20 16:11:27 +0200

    First commit

--- ComputerVisionApplication/ComputerVisionApplication/Services/EmotionService.cs
+++ ComputerVisionApplication/ComputerVisionApplication/Services/EmotionService.cs
@@ -4,8 +4,6 @@ using System.IO;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
-using CognitiveServices.Models;
-using ComputerVisionApplication.Models;
 using ComputerVisionApplication.Models.Emotion;
 using Newtonsoft.Json;
 
